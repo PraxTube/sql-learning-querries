cb3f3eaa7174f65cf0fd05b02d89cd9885d2b40c
PraxTube <lukinator28@gmail.com>
2024-06-14 21:53:31 +0200

    feat: Complete third task

--- query.sql
+++ query.sql
@@ -1,9 +1,1 @@
-SELECT 
-    CAST(SUM(Torhueter) AS INT128) AS "Anzahl Torhueter",
-    CAST(SUM(Verteidiger) AS INT128) AS "Anzahl Verteidiger",
-    CAST(SUM(Mittelfeldspieler) AS INT128) AS "Anzahl Mittelfeldspieler",
-    CAST(SUM(Stuermer) AS INT128) AS "Anzahl Stuermer"
-FROM 
-    Spieler
-WHERE 
-    Familienname LIKE 'A%';
+select Mannschafts_ID, Mannschaftsname, Mannschaftscode from Mannschaften where Regionname = 'South America' order by Mannschafts_ID desc limit 10;
