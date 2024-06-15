94b7bb08226c8c6d7d7073ce87a02a13bff619b1
PraxTube <lukinator28@gmail.com>
2024-06-15 08:07:02 +0200

    feat: Complete task 8
    Also started using formatter [pg_format](https://github.com/darold/pgFormatter).

--- query.sql
+++ query.sql
@@ -1,12 +1,19 @@
-select 
-    K.Konfoederationscode, 
-    count(*) AS 'Anzahl an Schiedsrichter'
-from 
-    Schiedsrichter S
-inner join 
-    Konfoederationen K 
-on 
-    S.Konfoederations_ID = K.Konfoederations_ID
-group by 
-    K.Konfoederationscode
-order by K.Konfoederationscode desc;
+SELECT
+    S.Spieler_ID,
+    S.Familienname,
+    S.Vorname,
+    count(*) AS Auftritte
+FROM
+    Spieler S
+    INNER JOIN Spielerauftritte A ON S.Spieler_ID = A.Spieler_ID
+GROUP BY
+    S.Spieler_ID,
+    S.Familienname,
+    S.Vorname
+HAVING
+    count(*) > 5
+ORDER BY
+    S.Spieler_ID DESC,
+    Auftritte ASC
+LIMIT 10;
+
